9fb6c5c77a1ba9633571a329df6b17c82d024473
jsaidoru <129057739+jsaidoru@users.noreply.github.com>
2025-11-18 19:52:02 +0700

    chỉnh script để giữ cửa sổ mở

--- run.sh
+++ run.sh
@@ -3,4 +3,7 @@ clear
 echo -e "Enter your filename to compile (for 580VNX only):"
 read name
 clear
-python ./580vnx/compiler_.py -f hex < "./rsc_ropchain/$name.rsc"
+python ./580vnx/compiler_.py -f hex < ./580vnx/$name # giả sử file asm trong folder 580vnx
+while true; do
+    sleep 10
+done
